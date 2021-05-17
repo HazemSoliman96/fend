1bda30cac1742156aad653b7b207b5ef8e1c4150
Hazem Soliman <hazemsoliman015@gmail.com>
2021-05-17 05:06:48 +0200

    webpack configuration

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -1,26 +1,34 @@
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+      library: "Client",
+      libraryTarget: "var",
+      path: path.resolve(__dirname, "dist"),
+    },
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+      rules: [
+        {
+          test: "/.js$/",
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+            title: 'Evaluate articles with NLP'
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,23 +1,45 @@
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+      libraryTarget: 'var',
+      library: 'Client',
+      path: path.resolve(__dirname, 'dist')
+    },
     mode: 'production',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+      rules: [
+        {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        }
+    ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+            title: 'Evaluate articles with NLP'
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [
+        `...`,
+        // example from https://www.npmjs.com/package/css-minimizer-webpack-plugin
+        new CssMinimizer(),
+      ],
+    },
 }
